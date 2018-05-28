1a789b2ac0296a89b83b5b4af7e3cb7b09de159c
oddrock <oddrock@gmail.com>
2018-05-28 14:33:33 +0800

    增加文件解压缩功能

--- src/main/java/com/oddrock/mail/mailrcvr/MailRcvr.java
+++ src/main/java/com/oddrock/mail/mailrcvr/MailRcvr.java
@@ -18,12 +18,23 @@ import com.oddrock.common.office.word.WordUtils;
 import com.oddrock.common.pdf.PdfUtils;
 import com.oddrock.common.pic.PicUtils;
 import com.oddrock.common.prop.Prop;
+import com.oddrock.common.zip.UnZipUtils;
 
 public class MailRcvr {
 	// 将邮件附件内容解析出来保存到同名txt文件
 	private static void parseMailAttachFile2TxtFile(MailRecv mail) throws IOException, XmlException, OpenXML4JException{
-		for(MailRecvAttach attach : mail.getAttachments()){
+		/*for(MailRecvAttach attach : mail.getAttachments()){
 			parseFile2TxtFile(attach.getLocalFilePath());
+		}*/
+		if(mail.getAttachments().size()>0){
+			MailRecvAttach firstAttach = mail.getAttachments().get(0);
+			String firstAttachPath =  firstAttach.getLocalFilePath();
+			File dir = new File(firstAttachPath).getParentFile();
+			for(File file : dir.listFiles()){
+				if(file.exists() && file.isFile() && !file.isHidden()){
+					parseFile2TxtFile(file.getCanonicalPath());
+				}
+			}
 		}
 	}
 	
@@ -52,6 +63,21 @@ public class MailRcvr {
 			PicUtils.createTxtFileFromPic(file);
 		}
 	}
+	
+	private static void unzipMailAttachFile(MailRecv mail) throws IOException, Exception{
+		for(MailRecvAttach attach : mail.getAttachments()){
+			unzipMailAttachFile(attach.getLocalFilePath());
+		}
+	}
+	
+	private static void unzipMailAttachFile(String filePath) throws IOException, Exception{
+		if(!FileUtils.fileExists(filePath)) return;
+		File file = new File(filePath);
+		if(UnZipUtils.canDeCompress(file.getCanonicalPath())) {
+			UnZipUtils.deCompress(file.getCanonicalPath(), file.getParentFile().getCanonicalPath());
+			FileUtils.gatherAllFiles(file.getParentFile().getCanonicalPath());
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
 		String server = Prop.get("mail.popserver");
@@ -61,12 +87,16 @@ public class MailRcvr {
 		String rejectAddresses = Prop.get("mail.rejectAddresses");
 		Pop3Config pop3Config = new Pop3Config(server, account, passwd, localAttachDirPath, rejectAddresses);
 		List<MailRecv> mailList = new Pop3MailRcvr().rcvMail(pop3Config);
+		if(Prop.getBool("mail.attach.unzip")){
+			for(MailRecv mail : mailList){
+				unzipMailAttachFile(mail);
+			}
+		}
 		if(Prop.getBool("mail.attach.file2txt")){
 			for(MailRecv mail : mailList){
 				parseMailAttachFile2TxtFile(mail);
 			}
 		}
-		
 	}
 
 }
